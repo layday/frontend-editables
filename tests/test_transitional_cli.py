80325061d7ef57ce1d5b2bc3d4eb5ee67137eea3
layday <layday@protonmail.com>
2021-06-12 02:55:28 +0300

    Fix CLI test

--- tests/test_transitional_cli.py
+++ tests/test_transitional_cli.py
@@ -1,5 +1,6 @@
 import os.path
 import subprocess
+import sys
 import sysconfig
 import venv
 
@@ -9,7 +10,11 @@ def test_self_install_from_path_with_default_settings(tmp_path):
     test_env = venv.EnvBuilder(with_pip=True)
     test_env.create(venv_directory)
     paths = sysconfig.get_paths(vars={"base": venv_directory})
-    python_executable = os.path.join(paths["scripts"], "python")
+    python_executable = os.path.join(paths["scripts"], os.path.basename(sys.executable))
+
+    if sys.version_info < (3, 7):
+        # Old pip version does not support PEP 517.
+        subprocess.check_call([python_executable, "-m", "pip", "install", "-U", "pip"])
     subprocess.check_call([python_executable, "-m", "pip", "install", "typing-extensions"])
     subprocess.check_call(
         [
