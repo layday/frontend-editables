4b186090e9fdc527903c036821af8e097b94d157
layday <layday@protonmail.com>
2021-06-13 14:06:47 +0300

    Add Python 3.10 to the test matrix

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def test(session: nox.Session):
     session.install(".[test]")
     session.run("coverage", "run", "-m", "pytest")
